4a49758fb903bde1c271f56e4a45c3bc9e74263d
mypeopoly <mypeopoly@gmail.com>
2024-04-17 15:56:26 +0800

    :zap: add Line_Purge.cfg

--- update.sh
+++ update.sh
@@ -4,11 +4,9 @@
 update_os() {
     echo "Updating OS to newer version..."
     
-    # 安装Qt相关的开发包
     echo "Installing Qt development packages..."
     echo 'armbian' | sudo -S apt-get install -y qtbase5-dev qtchooser qt5-qmake qtbase5-dev-tools libqt5serialport5 libqt5serialport5-dev
     
-    # 检查上一条命令是否成功执行
     if [ $? -ne 0 ]; then
         echo "Failed to install Qt development packages. Exiting..."
         exit 1
@@ -16,17 +14,14 @@ update_os() {
     
     echo "Qt development packages installed successfully."
     
-    # 在这里添加文件拷贝命令
-    # 例子: sudo cp /path/to/source /path/to/destination
     echo "Copying necessary files..."
-    # 确保替换下面的路径为你实际的文件路径
-    # sudo cp /path/to/source /path/to/destination
     cd /home/pi/magnetox-os-update
     cp auto-uuid/*  /home/pi/auto-uuid/
     chmod +x /home/pi/auto-uuid/*.sh
     chmod +x /home/pi/auto-uuid/MagnetoWifiHelper
     chmod +x /home/pi/auto-uuid/Magmotor
     cp config/* /home/pi/printer_data/config/
+    cp config/Line_Purge.cfg /home/pi/printer_data/config/KAMP/
     cp KlipperScreen/* /home/pi/KlipperScreen/panels/
     echo "Files copied successfully."
     echo 'armbian' | sudo sync
@@ -34,15 +29,13 @@ update_os() {
     echo 'armbian' | sudo reboot
 }
 
-# 第一步: 从URL获取版本号
+
 current_version=$(curl -s http://127.0.0.1:8880/get_os_version | jq -r '.version')
 echo "Current version from URL: $current_version"
 
-# 第二步: 从文件读取版本号
 file_version=$(cat /home/pi/magnetox-os-update/version.txt)
 echo "Version from file: $file_version"
 
-# 提取具体的版本号字符串，例如v1.1.0或v1.1.1
 version_from_url=$(echo $current_version | grep -o 'v[0-9]\+\.[0-9]\+\.[0-9]\+')
 version_from_file=$(echo $file_version | grep -o 'v[0-9]\+\.[0-9]\+\.[0-9]\+')
 
